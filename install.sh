cedecff0816ef8c9b79a38a434700827fd9a4e08
Joan <39315631+JLMSC@users.noreply.github.com>
2025-12-16 08:45:51 -0300

    Add install script for package installation

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+
+# (nonfree) For packages that don't have free licenses.
+# (multilib) Provides 32-bit packages as a compatibility layer inside a 64-bit system.
+# (multilib/nonfree) Provides additional 32-bit packages which have non-free licenses.
+echo ">> Enabling multilib/nonfree Repositories"
+sudo xbps-install -S void-repo-nonfree
+sudo xbps-install -S void-repo-multilib void-repo-multilib-nonfree
+
+echo ">> Installing Packages (packages.txt)"
+sudo xbps-install -S $(grep -v '^#' packages.txt)
+
+echo ">> Synchronizing Packages"
+sudo xbps-install -S
+
+echo ">> Done"
