f947f3b49452b8e0b1aab6edc4e4eb22f592f028
JLMSC <jlmsc314@gmail.com>
2025-12-17 08:12:22 -0300

    Remaining symlinks

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -25,5 +25,13 @@ link() {
 
 printf "\n. Creating Symlinks\n"
 link "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
+link "$DOTFILES_DIR/picom/picom.conf" "$HOME/.config/picom/picom.conf"
 link "$DOTFILES_DIR/clipmenu/config" "$HOME/.config/clipmenu/config"
+link "$DOTFILES_DIR/polybar/config.ini" "$HOME/.config/polybar/config.ini"
+link "$DOTFILES_DIR/polybar/scripts/clipboard.sh" "$HOME/.config/polybar/scripts/clipboard.sh"
+link "$DOTFILES_DIR/i3/config" "$HOME/.config/i3/config"
+link "$DOTFILES_DIR/.icons/default/index.theme" "$HOME/.icons/default/index.theme"
+link "$DOTFILES_DIR/.Xresources" "$HOME/.Xresources"
+link "$DOTFILES_DIR/.profile" "$HOME/.profile"
+link "$DOTFILES_DIR/.config/gtk-3.0/settings.ini" "$HOME/.config/gtk-3.0/settings.ini"
 echo ". Done"
